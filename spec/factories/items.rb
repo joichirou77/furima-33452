66c78c430586d61320653fc5d36b0015e92a6b29
joichirou <jou.2945@gmail.com>
2021-02-05 16:45:46 +0900

    テストコード実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name           {"test"}
+    description     {"test"}
+    category_id     {"2"}
+    condition_id       {"2"}
+    postage_payer_id  {"2"}
+    prefecture_id     {"2"}
+    preparation_day_id {"2"}
+    price    {"400"}
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png', content_type: 'image/png')
+    end
   end
 end
